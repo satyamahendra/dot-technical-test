273d388ef351c0c52f509622554aa2187227b982
satya mahendra <satyamahendra09@gmail.com>
2024-10-06 12:20:21 +0700

    fix: minor layout responsiveness adjustment

--- src/app/(protected)/layout.tsx
+++ src/app/(protected)/layout.tsx
@@ -8,7 +8,7 @@ export default async function ProtectedLayout({
 }>) {
     return (
         <div className="w-screen sm:h-screen flex sm:flex-row flex-col sm:items-center sm:justify-center font-[family-name:var(--font-geist-sans)]">
-            <main className="flex sm:flex-row flex-col sm:h-3/5 duration-200">
+            <main className="flex sm:3/4 md:w-4/5 lg:w-2/3 xl:w-1/3 sm:flex-row flex-col sm:h-3/5 duration-200">
                 <div className="sm:w-1/3 border-r px-4 flex-1 h-full flex flex-col pt-4">
                     <Navbar />
                 </div>
