f746c1edd7de60e94181d38aa20f2cfe9f2e05b7
satya mahendra <satyamahendra09@gmail.com>
2024-10-06 14:17:40 +0700

    fix: redirect if no session

--- src/app/(protected)/layout.tsx
+++ src/app/(protected)/layout.tsx
@@ -1,4 +1,6 @@
 import Navbar from "@/components/Navbar";
+import { validateRequest } from "@/lib/lucia/validate-request";
+import { redirect } from "next/navigation";
 
 
 export default async function ProtectedLayout({
@@ -6,6 +8,13 @@ export default async function ProtectedLayout({
 }: Readonly<{
     children: React.ReactNode;
 }>) {
+
+    const { session } = await validateRequest();
+
+    if (!session) {
+        redirect("/auth")
+    }
+
     return (
         <div className="w-screen sm:h-screen flex sm:flex-row flex-col sm:items-center sm:justify-center font-[family-name:var(--font-geist-sans)]">
             <main className="flex sm:3/4 md:w-4/5 lg:w-2/3 xl:w-1/3 sm:flex-row flex-col sm:h-3/5 duration-200">
